703c692368f6a99d138b203880604225677cf7ba
MTalha250 <talhabinay@gmail.com>
2025-08-08 23:12:33 +0500

    update

--- server/build.sh
+++ server/build.sh
@@ -3,3 +3,9 @@ python3 -m pip install -r requirements.txt
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
+
+# Vercel static output expects "staticfiles_build" per vercel.json
+mkdir -p staticfiles_build
+if [ -d "staticfiles" ]; then
+  cp -r staticfiles/. staticfiles_build/
+fi
