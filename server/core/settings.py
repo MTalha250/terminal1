04cc4aad83876c709faee2586dcade30e0406d38
MTalha250 <talhabinay@gmail.com>
2025-08-09 01:07:00 +0500

    update

--- server/core/settings.py
+++ server/core/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
