4b42e9462f5cec2748e6b85f86cd362852729b61
MTalha250 <talhabinay@gmail.com>
2025-08-09 02:00:19 +0500

    update

--- server/api/views.py
+++ server/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import status, serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
 from .models import Inquiry
 
 # Create your views here.
@@ -22,7 +21,6 @@ class InquirySerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'created_at')
 
 
-@csrf_exempt
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def create_inquiry(request):
