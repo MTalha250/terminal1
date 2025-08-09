906d64e21f9bffd54e60cee8287cee003f9df02b
MTalha250 <talhabinay@gmail.com>
2025-08-09 14:42:44 +0500

    update

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-    path('inquiries/', csrf_exempt(views.create_inquiry), name='create_inquiry'),
-    path('diag/', views.diag, name='diag'),
+    path('inquiries/', views.create_inquiry, name='create_inquiry'),
 ]
